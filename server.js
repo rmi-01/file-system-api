94cc84330bf219d3cf6e2a1d5759a7b37741c096
ebuildSlayer <mohammadibrahim.ibrahim9@gmail.com>
2020-04-21 16:41:01 +0500

    ..

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const db = knex({
   client: 'pg',
   connection: {
   	connectionString: process.env.DATABASE_URL,
-  	process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0",
-  	ssl: true
+  	process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 }
 });
 
